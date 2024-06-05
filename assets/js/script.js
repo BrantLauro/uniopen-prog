395813bd8fa8de75684580f2ae805b1537d0734d
BrantLauro <brantlauro@gmail.com>
2024-06-05 14:32:26 -0300

    refatoração de código

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,8 @@
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-
-
-// sidebar variables
-//const sidebar = document.querySelector("[data-sidebar]");
-//const sidebarBtn = document.querySelector("[data-sidebar-btn]");
-
-// sidebar toggle functionality for mobile
-//sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
-
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+// depoimentos variables
+const depoimentosItem = document.querySelectorAll("[data-depoimentos-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -28,30 +17,30 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const depoimentosModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+for (let i = 0; i < depoimentosItem.length; i++) {
 
-  testimonialsItem[i].addEventListener("click", function () {
+  depoimentosItem[i].addEventListener("click", function () {
 
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalImg.src = this.querySelector("[data-depoimentos-avatar]").src;
+    modalImg.alt = this.querySelector("[data-depoimentos-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-depoimentos-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-depoimentos-text]").innerHTML;
 
-    testimonialsModalFunc();
+    depoimentosModalFunc();
 
   });
 
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+modalCloseBtn.addEventListener("click", depoimentosModalFunc);
+overlay.addEventListener("click", depoimentosModalFunc);
 
 
 // page navigation variables
